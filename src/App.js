d48c33a7d759ba3d2be7247af535d197da644e46
Maxim <panda.gtr007@gmail.com>
2022-11-30 23:09:53 +0600

    add arr sneakers and render at page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import Header from './components/Header';
 import Drawer from './components/Drawer';
 import Card from './components/Card';
 
-
+const arr = [
+  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '12 999', imageUrl: '/img/sneakers/1.jpg'},
+  {title: 'Мужские Кроссовки Nike Air Max 270', price: '12 999', imageUrl: '/img/sneakers/2.jpg'},
+  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '8 499', imageUrl: '/img/sneakers/3.jpg'},
+  {title: 'Кроссовки Puma X Aka Boku Future Rider', price: '8 999', imageUrl: '/img/sneakers/4.jpg'},
+  // {name: '', price: '', imageUrl: ''}
+]
 function App() {
   return (
-
     <div className="wrapper">
-    
       <Drawer/>
-
       <Header/>
-
       {/* ТЕЛО ПОСЛЕ ХЕАДЕРА */}
       <div className="content p-40">
-     
           <div className="content__title d-flex mb-40 justify-between">
             <h1 className="">Все кроссовки</h1>
             <div className="content__search">
@@ -22,18 +23,21 @@ function App() {
               <input placeholder="Поиск..." type="text" />
             </div>
           </div>
-
           {/* КАРТОЧКИ */}
         <div className="content__cards d-flex">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+       
+          {arr.map((obj) => ( //можно было сделать forEach - но он ничего не возвращает
+            <Card 
+              title = {obj.title} 
+              price= {obj.price} 
+              imageUrl={obj.imageUrl}
+            />
+          ))}
         </div>
 
 
       </div>
-    </div> //?-----wrapper
+    </div>
    
 
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
-function Card(){
+function Card(props){
     return (
       <div className="card">
         <div className="card__favorite">
           <img src="img/content/heart-unliked.svg" alt="" />
         </div>
         <div className="card__img">
-          <img src="/img/sneakers/1.jpg" alt="sneaker" />
+          <img src={props.imageUrl} alt="sneaker" />
         </div>
-        <h5 className="card__text">Мужские Кроссовки Nike Blazer Mid Suede</h5>
+        <h5 className="card__text">{props.title}</h5>
         <div>
           <div className="card__info d-flex justify-between align-center">
             <div className="card__price d-flex flex-column">
               <span>Цена:</span>
-              <b>12999 руб.</b>
+              <b>{props.price} руб.</b>
             </div>
             <button>
               <img className="card__plus" src="/img/content/plus.svg" alt="Plus"/>
