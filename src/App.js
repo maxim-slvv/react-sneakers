291bd5f2da07b574d3166cb68e886825f3e2f8f1
Maxim <panda.gtr007@gmail.com>
2022-12-03 19:45:14 +0600

    now get the data once

--- src/App.js
+++ src/App.js
@@ -5,77 +5,22 @@ import Card from './components/Card';
 
 
 function App() {
-  const [items, setItems] = React.useState(
-    [
-      {
-       "title": "Мужские Кроссовки Nike Blazer Mid Suede",
-       "price": "12 999",
-       "imageUrl": "/img/sneakers/1.jpg"
-      },
-      {
-       "title": "Мужские Кроссовки Nike Air Max 270",
-       "price": "12 999",
-       "imageUrl": "/img/sneakers/2.jpg"
-      },
-      {
-       "title": "Мужские Кроссовки Nike Blazer Mid Suede",
-       "price": "8 499",
-       "imageUrl": "/img/sneakers/3.jpg"
-      },
-      {
-       "title": "Кроссовки Puma X Aka Boku Future Rider",
-       "price": "8 999",
-       "imageUrl": "/img/sneakers/4.jpg"
-      },
-      {
-       "title": "Мужские Кроссовки Under Armour Curry 8",
-       "price": "15 199",
-       "imageUrl": "/img/sneakers/5.jpg"
-      },
-      {
-       "title": "Мужские Кроссовки Nike Kyrie 7",
-       "price": "11 299",
-       "imageUrl": "/img/sneakers/6.jpg"
-      },
-      {
-       "title": "Мужские Кроссовки Jordan Air Jordan 11",
-       "price": "10 799",
-       "imageUrl": "/img/sneakers/7.jpg"
-      },
-      {
-       "title": "Мужские Кроссовки Nike LeBron XVIII",
-       "price": "16 499",
-       "imageUrl": "/img/sneakers/8.jpg"
-      },
-      {
-       "title": "Мужские Кроссовки Nike Lebron XVIII Low",
-       "price": "13 999",
-       "imageUrl": "/img/sneakers/9.jpg"
-      },
-      {
-       "title": "Мужские Кроссовки Nike Blazer Mid Suede",
-       "price": "8 499",
-       "imageUrl": "/img/sneakers/1.jpg"
-      },
-      {
-       "title": "Кроссовки Puma X Aka Boku Future Rider",
-       "price": "8 999",
-       "imageUrl": "/img/sneakers/2.jpg"
-      },
-      {
-       "title": "Мужские Кроссовки Nike Kyrie Flytrap IV",
-       "price": "11 299",
-       "imageUrl": "/img/sneakers/10.jpg"
-      }
-     ]
-  );
-  const [cartOpened, setCartOpened] = React.useState(false)
+  const [items, setItems] = React.useState([]);
 
+  React.useEffect(() => {
+    fetch('https://638b00687220b45d2285fede.mockapi.io/items').then((res) => {
+      return res.json()
+    }).then(json => {
+      setItems(json)
+    })
+  }, [])
+ 
+
+  const [cartOpened, setCartOpened] = React.useState(false)
   return (
     <div className="wrapper">
       {cartOpened && <Drawer onClose={()=> setCartOpened(false)}/>}
       <Header onClickCart={()=> setCartOpened(true)}/>
-
       {/* ТЕЛО ПОСЛЕ ХЕАДЕРА */}
       <div className="content p-40">
           <div className="content__title d-flex mb-40 justify-between">
@@ -87,7 +32,6 @@ function App() {
           </div>
           {/* КАРТОЧКИ */}
         <div className="content__cards d-flex">
-       
           {items.map((obj) => ( //можно было сделать forEach - но он ничего не возвращает
             <Card 
               title = {obj.title} 
